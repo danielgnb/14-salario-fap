051866a717109c5a99b5da03678cc4f7b2dcea85
Daniel <daniel.nevesb@hotmail.com>
2024-01-12 01:02:02 -0300

    Tela de Gestão de Funcionário desenvolvida

---
+++ Desenvolvimento [PESSOAL]/Ronaldo [DEV PESSOAL]/dashboard_14_salario/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import AppRoutes from './routes.js'
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes/>
+    </div>
+  );
+}
+
+export default App;
